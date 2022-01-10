de31575be875c680b0647ab99a00cc53f42e9895
Vladislav Mazikov <vladislavmazikov@yandex.ru>
2022-01-10 21:47:27 +0700

    feat: реализовать добавление карточек на страницу через template

--- scripts/index.js
+++ scripts/index.js
@@ -37,3 +37,43 @@ popupOverlay.addEventListener('click', function() {
   jobInput.value = profileSubtitle.textContent;
 });
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+// Кусок кода отвечающий за стартовое добавление карточек на страницу
+const cardContainer = document.querySelector('.cards');
+
+const cardTemplate = document.querySelector('#cards').content;
+
+initialCards.forEach(function (item) {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+  card.querySelector('.card__title').textContent = item.name;
+  card.querySelector('.card__image').src = item.link;
+  cardContainer.append(card);
+});
+
+
+
