4838cad7f4c00079ba99c4dd665ff0b46a3322f8
Vladislav Mazikov <vladislavmazikov@yandex.ru>
2022-02-08 01:59:51 +0700

    feat: Расширить скрипт для всех инпутов

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,31 +1,61 @@
-const formElement = document.querySelector('.form-add-card');
-const formInput = formElement.querySelector('.form__input-text');
-const formError = formElement.querySelector(`.${formInput.id}-error`);
-
-const showError = (input, errorMessage) => {
-  input.classList.add('form__input-text_type_error');
-  formError.classList.add('form__input-error_active');
-  formError.textContent = errorMessage;
+//Функция показа ошибки
+const showErrorInput = (formItem, inputItem, errorMessage) => {
+  const inputError = formItem.querySelector(`.${inputItem.id}-error`);
+  inputItem.classList.add('form__input_type_error');
+  inputError.textContent = errorMessage;
+  inputError.classList.add('form__input-error_active');
 };
-
-const checkInputValidity = () => {
-  if(!formInput.validity.valid) {
-    showError(formInput, formInput.validationMessage);
+//Функция скрытия ошибки
+const hideErrorInput = (formItem, inputItem) => {
+  const inputError = formItem.querySelector(`.${inputItem.id}-error`);
+  inputItem.classList.remove('form__input_type_error');
+  inputError.textContent = ' ';
+  inputError.classList.remove('form__input-error_active');
+};
+//Проверка валидности полей заполнения
+const checkInputValidity = (formItem, inputItem) => {
+  if (!inputItem.validity.valid) {
+    showErrorInput(formItem, inputItem, inputItem.validationMessage);
   } else {
-    hideError(formInput);
-  }
+    hideErrorInput(formItem, inputItem);
+  };
 };
-
-const hideError = (input) => {
-  input.classList.remove('form__input-text_type_error');
-  formError.classList.remove('form__input-error_active');
-  formError.textContent = '';
+//Вешаем слушатели на инпуты
+const setEventListeners = (formItem) => {
+  const inputList = Array.from(formItem.querySelectorAll('.form__input'));
+  const button = formItem.querySelector('.form__button-submit');
+  toggleButton(inputList, button);
+  inputList.forEach((inputItem) => {
+    inputItem.addEventListener('input', () => {
+      checkInputValidity(formItem, inputItem);
+      toggleButton(inputList, button);
+    });
+  });
 };
-
-formElement.addEventListener('submit', function (evt) {
-  evt.preventDefault();
-});
-
-formInput.addEventListener('input', function () {
-  checkInputValidity();
-});
+//Проверка валидности формы
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+};
+//Возврат валидности полей
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputItem) => {
+    return !inputItem.validity.valid;
+  });
+};
+//Переключение Кнопки
+const toggleButton = (inputList, button) => {
+  if (hasInvalidInput(inputList)) {
+    button.classList.add('form__button-submit_disabled');
+    button.setAttribute('disabled', '');
+  } else {
+    button.classList.remove('form__button-submit_disabled');
+    button.removeAttribute('disabled');
+  };
+};
+enableValidation();
